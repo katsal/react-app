f492bd2c20aa0937629f159b31b1f08208c32870
Karthika Sanal <karthikasanal20@gmail.com>
2024-07-14 23:08:51 +1000

    feat: use logical and

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,16 +2,10 @@ function ListGroup() {
   let items = ["Melbourne", "Sydney", "London", "Tokyo", "Kochi"];
   items = [];
 
-  // const message = items.length === 0 ? <p>No item found</p> : null;
-
-  const getMessage = () => {
-    return items.length === 0 ? <p>No item found</p> : null;
-  };
-
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
