40e087dbcc3e359c3f956ff9a3f11469ed17ef63
Karthika Sanal <karthikasanal20@gmail.com>
2024-07-14 23:07:21 +1000

    feat: use function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,12 +2,16 @@ function ListGroup() {
   let items = ["Melbourne", "Sydney", "London", "Tokyo", "Kochi"];
   items = [];
 
-  const message = items.length === 0 ? <p>No item found</p> : null;
+  // const message = items.length === 0 ? <p>No item found</p> : null;
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
 
   return (
     <>
       <h1>List</h1>
-      {message}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
