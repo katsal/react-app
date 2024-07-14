ee1c506520117e7b50ff0095726b025edf1a4538
Karthika Sanal <karthikasanal20@gmail.com>
2024-07-14 23:04:42 +1000

    feat: use ternary operator

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,11 @@
 function ListGroup() {
-  const items = ["Melbourne", "Sydney", "London", "Tokyo", "Kochi"];
+  let items = ["Melbourne", "Sydney", "London", "Tokyo", "Kochi"];
+  items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
