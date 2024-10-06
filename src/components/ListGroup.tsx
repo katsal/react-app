0e32c9c5955cf34a871f9b5d857c4429b4691b6f
Karthika Sanal <karthikasanal20@gmail.com>
2024-10-06 23:15:54 +1100

    feat:event handler

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,16 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Melbourne", "Sydney", "London", "Tokyo", "Kochi"];
-
+  //event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={() => console.log(item, index)}
-          >
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
