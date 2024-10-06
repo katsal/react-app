cfb6e990fc5061b12f51b97eb3ccf87aba734f7b
Karthika Sanal <karthikasanal20@gmail.com>
2024-10-06 23:35:27 +1100

    feat: select one item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,6 +2,8 @@ import { MouseEvent } from "react";
 
 function ListGroup() {
   let items = ["Melbourne", "Sydney", "London", "Tokyo", "Kochi"];
+  let selectedIndex = 0;
+
   //event handler
   const handleClick = (event: MouseEvent) => console.log(event);
   return (
@@ -10,7 +12,15 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={handleClick}
+          >
             {item}
           </li>
         ))}
